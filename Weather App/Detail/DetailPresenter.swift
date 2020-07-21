ab4ed54d1bdb14f32928ce3895ef8dff1cd89d09
Eugene Zimin <eugene.zimin@stickypassword.com>
2020-07-21 12:21:13 +0300

    -fix detail screen appearance

--- Weather App/Detail/DetailPresenter.swift
+++ Weather App/Detail/DetailPresenter.swift
@@ -30,8 +30,31 @@ class DetailPresenter: NSObject
         super.init()
     }
     
+    func onSelection(_ vc: DetailViewController,_ indexPath:IndexPath)
+    {
+        let weatherData = self.interactor.locality.weatherData["daily"].arrayValue[indexPath.item]
+        
+        let url = URL(string: "https://openweathermap.org/img/wn/" + weatherData["weather"][0]["icon"].stringValue + "@2x.png")!
+        let processor = RoundCornerImageProcessor(cornerRadius: 20)
+        vc.imageView.kf.indicatorType = .activity
+        vc.imageView.kf.setImage(
+            with: url,
+            options: [
+                .processor(processor),
+                .transition(.fade(1)),
+                .cacheOriginalImage
+            ])
+        
+        let day = DateFormatter().weekdaySymbols[Calendar.current.component(.weekday, from: Calendar.current.date(byAdding: .day, value: indexPath.item + 1, to: Date())!) - 1].firstCapitalized
+        
+        vc.nameLabel.text = day
+        vc.tempLabel.text = String(format:"%+.f..%+.f",weatherData["temp"]["min"].floatValue, weatherData["temp"]["max"].floatValue) + " °C"
+        vc.descLabel.text = weatherData["weather"][0]["description"].stringValue.firstCapitalized
+    }
+    
     func bindTable(_ vc: DetailViewController)
     {
+
         guard vc.collectionView.dataSource == nil
         else
         {
@@ -47,35 +70,11 @@ class DetailPresenter: NSObject
         vc.collectionView.rx.itemSelected.subscribe(onNext:
         { indexPath in
             
-            let weatherData = self.interactor.locality.weatherData["daily"].arrayValue[indexPath.item]
-            
-            let url = URL(string: "https://openweathermap.org/img/wn/" + weatherData["weather"][0]["icon"].stringValue + "@2x.png")!
-            let processor = RoundCornerImageProcessor(cornerRadius: 20)
-            vc.imageView.kf.indicatorType = .activity
-            vc.imageView.kf.setImage(
-                with: url,
-                options: [
-                    .processor(processor),
-                    .transition(.fade(1)),
-                    .cacheOriginalImage
-                ])
-            
-            let day = DateFormatter().weekdaySymbols[Calendar.current.component(.weekday, from: Calendar.current.date(byAdding: .day, value: indexPath.item + 1, to: Date())!) - 1].firstCapitalized
-            
-            vc.nameLabel.text = day
-            vc.tempLabel.text = String(format:"%+.f..%+.f",weatherData["temp"]["min"].floatValue, weatherData["temp"]["max"].floatValue) + " °C"
-            vc.descLabel.text = weatherData["weather"][0]["description"].stringValue.firstCapitalized
+            self.onSelection(vc, indexPath)
             
         }).disposed(by: rxBag)
         
-        
-        DispatchQueue.main.async
-        {
-            vc.collectionView.allowsSelection = true
-            
-            vc.collectionView.selectItem(at: IndexPath(item: 0, section: 0), animated: true, scrollPosition: .left)
-        }
-
+        #warning("Not solved initial selection reactive way :(")
     }
     
     func onViewDidLoad(_ vc: DetailViewController)
@@ -85,6 +84,10 @@ class DetailPresenter: NSObject
         
         vc.collectionView.layer.borderWidth = 1 / UIScreen.main.nativeScale
         vc.collectionView.layer.borderColor = UIColor.gray.cgColor
+        
+        vc.imageView.superview?.isHidden = true
+        self.onSelection(vc, IndexPath(item: 0, section: 0))
+        vc.imageView.superview?.isHidden = false
     }
     
     func onViewAppear(_ vc: DetailViewController)
